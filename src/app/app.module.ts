315345f34ccaf7bf3f433c6e2927dafdf3d0b0f5
Daniel <danielptm@me.com>
2020-05-25 14:30:20 -0700

    Added a card with angular materials

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,17 +7,25 @@ import {MemeHttpService} from "./services/MemeHttp/meme-http.service";
 import {HttpClientModule} from "@angular/common/http";
 import { ThisWeekComponent } from './public/this-week/this-week.component';
 import { LastWeekComponent } from './public/last-week/last-week.component';
+import { CardComponent } from './ui/card/card.component';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import {MatButtonModule} from "@angular/material/button";
+import {MatIconModule} from "@angular/material/icon";
 
 @NgModule({
   declarations: [
     AppComponent,
     ThisWeekComponent,
-    LastWeekComponent
+    LastWeekComponent,
+    CardComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    NoopAnimationsModule,
+    MatButtonModule,
+    MatIconModule
   ],
   providers: [
     MemeHttpService
